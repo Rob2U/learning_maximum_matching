38e900ab541fc6a252d88f800e465bf234d7b79b
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-11-27 19:30:19 +0100

    implemented union find

---
+++ src/environment/union_find.py
@@ -0,0 +1,24 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u: int) -> int:
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u: int, v: int) -> None:
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+    def connected(self, u: int, v: int) -> bool:
+        return self.find(u) == self.find(v)
