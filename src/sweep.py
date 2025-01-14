259e272dd4d4a38481e8d2d79880e9eb963ec411
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2025-01-14 23:24:20 +0100

    fix sweep

--- src/sweep.py
+++ src/sweep.py
@@ -2,7 +2,11 @@ import subprocess
 import wandb
 import math
 
-# Load the sweep configuration
+# Usage:
+# - adjust hyperparameters to sweep over in sweep_config
+# - python src/sweep.py
+# - check that all agent subprocesses were started with `squeue -u <username>`
+
 sweep_config = {
     "program": "src/train.py",
     "entity": "na_mst_2",
@@ -12,36 +16,39 @@ sweep_config = {
     "parameters": {
         "learning_rate": {
             "distribution": "log_uniform",
-            "min": math.log(1e-5)
-            "max": math.log(1e-3)
+            "min": math.log(1e-5),
+            "max": math.log(1e-3),
         },
         "gamma": {
             "distribution": "log_uniform",
-            "min": math.log(0.9)
-            "max": math.log(0.999)
+            "min": math.log(0.9),
+            "max": math.log(0.999),
         },
     },
 }
 
 NUM_AGENTS = 5
 
-def start_agent(sweep_id):
+
+def start_agent(sweep_id: str) -> None:
     # Start the agent
     subprocess.run(
         [
             "sbatch",
-            "batch_sweep.sh",
+            "batch_agent.sh",
             sweep_id,
         ]
     )
 
 
-def sweep():
+def sweep() -> None:
     sweep_id = wandb.sweep(
         sweep_config,
         entity="na_mst_2",
         project="constrainedIS",    
     )
+
+    print("Sweep ID: ", sweep_id)
 
     for i in range(NUM_AGENTS):
         start_agent(sweep_id)
