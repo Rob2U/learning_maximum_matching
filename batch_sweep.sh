fae077cc0220cc7bf1332b90d1a6943bd735549b
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2025-01-14 17:01:15 +0100

    try fixing it

--- batch_sweep.sh
+++ batch_sweep.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-SWEEP_ID = wandb sweep ./configs/sweep.yaml
+SWEEP_ID=$(wandb sweep ./configs/sweep.yaml | grep "Created sweep with ID:" | awk '{print $NF}')
+
+echo "SWEEP_ID: $SWEEP_ID"
 
 # for loop
 for i in {1..5}
